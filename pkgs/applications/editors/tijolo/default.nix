6d597f4c6c91514f89cbc64204994215693ae027
sunder <evenquantity@gmail.com>
2023-12-27 14:18:32 +0300

    tijolo: 0.7.4 -> 0.8.0-alpha

--- pkgs/applications/editors/tijolo/default.nix
+++ pkgs/applications/editors/tijolo/default.nix
@@ -2,34 +2,57 @@
 , lib
 , fetchFromGitHub
 , crystal
-, vte
-, libgit2
-, editorconfig-core-c
-, gtksourceview4
-, wrapGAppsHook
+, vte-gtk4
+, gtksourceview5
+, wrapGAppsHook4
 , desktopToDarwinBundle
+, gi-crystal
+, pango
+, harfbuzz
+, libadwaita
+, gobject-introspection
+, libxml2
 }:
 crystal.buildCrystalPackage rec {
   pname = "tijolo";
-  version = "0.7.4";
+  version = "0.8.0-alpha";
 
   src = fetchFromGitHub {
     owner = "hugopl";
     repo = "tijolo";
-    rev = "v${version}";
-    hash = "sha256-3TfXvRVP3lu43qF3RWCHnZ3czTaSl5EzrhuTlpnMfKo=";
+    rev = "848b90e5e6071c2023aac91e9767d409a8f2dee6";
+    hash = "sha256-mv8EuKSrxMkf32ZK9th2K7XBsBHrSXsmsJGI9lv/N3Y=";
   };
 
-  nativeBuildInputs = [ wrapGAppsHook ]
-    ++ lib.optionals stdenv.isDarwin [ desktopToDarwinBundle ];
-  buildInputs = [ vte libgit2 gtksourceview4 editorconfig-core-c ];
+  nativeBuildInputs = [
+    wrapGAppsHook4
+    gi-crystal
+    gobject-introspection
+    libxml2
+  ] ++ lib.optionals stdenv.isDarwin [ desktopToDarwinBundle ];
 
-  buildTargets = [ "all" ];
-  doCheck = false;
+  buildInputs = [
+    libadwaita
+    vte-gtk4
+    gtksourceview5
+    harfbuzz
+    pango
+    gtksourceview5
+  ];
 
   shardsFile = ./shards.nix;
 
-  installTargets = [ "install" "install-fonts"];
+  buildPhase = ''
+    runHook preBuild
+
+    gi-crystal
+    shards build --release -s
+
+    runHook postBuild
+  '';
+  doCheck = false;
+
+  installTargets = [ "install" "install-fonts" "post-install" ];
   doInstallCheck = false;
 
   meta = with lib; {
--- pkgs/applications/editors/tijolo/shards.nix
+++ pkgs/applications/editors/tijolo/shards.nix
@@ -1,27 +1,42 @@
 {
-  compiled_license = {
-    url = "https://github.com/elorest/compiled_license.git";
-    rev = "v1.2.2";
-    sha256 = "1f412r6m31cc093lcw31m2rp5s3y7vh6q3wc3xh9b8vccvmj21p7";
-  };
   fzy = {
     url = "https://github.com/hugopl/fzy.git";
     rev = "v0.5.5";
     sha256 = "1zk95m43ymx9ilwr6iw9l44nkmp4sas28ib0dkr07hkhgrkw68sv";
   };
-  gobject = {
-    url = "https://github.com/jhass/crystal-gobject.git";
-    rev = "v0.10.0";
-    sha256 = "02cc3486ifbffdbvgl75ylwn47gpfb2j0srz92jx5yz1d596x746";
+  # gio = {
+  #   url = "https://github.com/hugopl/gio.cr.git";
+  #   rev = "v0.2.1";
+  #   sha256 = "0vl5lpvhhdi9pvyl12smag0i3dwj1jjz1zzilc8ai0wjrnbnwlxs";
+  # };
+  gtk4 = {
+    url = "https://github.com/hugopl/gtk4.cr.git";
+    rev = "v0.16.0";
+    sha256 = "1bnnfy07246vwbjfwhziv2yw1g52pzhhrr25660fp3qcnw8q513w";
+  };
+  harfbuzz = {
+    url = "https://github.com/hugopl/harfbuzz.cr.git";
+    rev = "v0.2.0";
+    sha256 = "06wgqxwyib5416yp53j2iwcbr3bl4jjxb1flm7z103l365par694";
+  };
+  libadwaita = {
+    url = "https://github.com/geopjr/libadwaita.cr.git";
+    rev = "cffabb56e911d2a90c53c2fd14d6bd08bf5ac446";
+    sha256 = "0gcq04vgxg0vff9fcalgzq9phq0y76diihkzhlfn91bdxwkx7rl3";
+  };
+  pango = {
+    url = "https://github.com/hugopl/pango.cr.git";
+    rev = "v0.3.1";
+    sha256 = "0xlf127flimnll875mcq92q7xsi975rrgdpcpmnrwllhdhfx9qmv";
   };
   toml = {
     url = "https://github.com/crystal-community/toml.cr.git";
     rev = "v0.7.0";
     sha256 = "0vznx2irvx6m8w6chdf9ms72n2w0cf2m5cwh0cjmp1jz9y3xwv7z";
   };
-  version_from_shard = {
-    url = "https://github.com/hugopl/version_from_shard.git";
-    rev = "v1.2.5";
-    sha256 = "0xizj0q4rd541rwjbx04cjifc2gfx4l5v6q2y7gmd0ndjmkgb8ik";
+  vte = {
+    url = "https://github.com/hugopl/vte.cr.git";
+    rev = "v0.1.0";
+    sha256 = "18x2l9cm8czhwpdfng3rz00kn07c49y0555kf2j5l2wb0g05snh3";
   };
 }
